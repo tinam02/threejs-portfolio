515c3d337f7dec7cb2a9c0a0238e5408a26d29b2
tina <oge612@gmail.com>
2022-04-20 12:12:27 +0200

    counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useEffect, useRef, useState } from "react";
 
 import Layout from "./Components/Layout";
 import Cursor from "./Components/Cursor";
@@ -10,8 +10,22 @@ import P404 from "./Pages/P404";
 // const Layout = lazy(() => import('./Components/Layout'))
 
 function App() {
+  const countRef = useRef();
+  const [view, setView] = useState();
+  useEffect(() => {
+    const updateCount = async () => {
+     await fetch("https://api.countapi.xyz/update/tina-m.vercel.app/count/?amount=1")
+        .then((res) =>  res.json())
+        .then((res) => {
+          setView(res.value);  
+        });
+    };
+    updateCount();console.log(view);
+    }, []);
+
   return (
     <div className="App">
+      <div style={{opacity:0}} ref={countRef}>{view}</div>
       <Cursor />
       <Routes>
         <Route path="/" element={<Layout />} />
